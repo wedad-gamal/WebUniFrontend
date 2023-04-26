dc17dbe69636ed19afef48282dd02546d1397c80
Wedad <wedad.gamal@gmail.com>
2023-04-26 17:49:44 +0200

    Add project files.

---
+++ WebUniFrontend/Controllers/CourseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUniFrontend.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+    }
+}
